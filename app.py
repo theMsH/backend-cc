dc62f777d25d466f3c4413c9136d41917d045299
Miika <123482448+theMsH@users.noreply.github.com>
2025-03-10 19:49:26 +0200

    change version number

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<h1>Sentiment backend v1</h1>"
+    return "<h1>Sentiment backend v1.1</h1>"
 
 @app.route("/sentiment", methods=["POST"])
 def get_sentiment():
